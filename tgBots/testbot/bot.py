32086148e183add8c5481fe2c6098a609559d647
Alex <a1eksbend@gmail.com>
2022-11-06 00:14:27 +0300

    п

--- tgBots/testbot/bot.py
+++ tgBots/testbot/bot.py
@@ -1,15 +1,27 @@
-from email import message
-from aiogram import Bot, Dispatcher, types, executor
-from config import TOKEN_API
+from aiogram.types import Message#импортируем тип message
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
+from config import TOKEN_API
+from config import admin_id# для того чтобы себе отпрвлять сообщения
+import asyncio
+
+loop = asyncio.get_event_loop()#Создаём поток где будут обрабатываться все события
 bot = Bot(TOKEN_API)
 # Диспетчер
-dp = Dispatcher(bot)
+#dp = Dispatcher(bot)# экземпляр класса
+dp = Dispatcher(bot, loop=loop)#Создаём обработчик и доставщик/ Берём бота и полученный поток
+
+
+async def send_to_admin(dp):
+    await bot.send_message(chat_id = admin_id, text = 'Бот запущен')
 
 
 @dp.message_handler()# ответ - текст пользователя в верхнем регистре
-async def echo_capitalize(message: types.Message):
-    await message.answer(text=message.text.capitalize())
+async def echo_upper(message: types.Message):#принимаем сообщение
+    if message.text.count(' ') >= 1:# ответит ответным сообщением если в приёмном сообщении будет хотя бы один пробел
+        await message.answer(text=message.text.upper())# ответ
 
 if __name__ =='__name__':
-    executor.start_polling(dp)
+    executor.start_polling(dp)#polling запрос сервера на обновлние данных
